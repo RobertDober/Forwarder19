1e74b2c728e037b21a19b00bffe2dd0de80215e1
Robert Dober <robert.dober@gmail.com>
2012-04-20 20:11:10 +0200

    dead commented code removed

--- lib/forwarder/params.rb
+++ lib/forwarder/params.rb
@@ -24,59 +24,6 @@ module Forwarder
       compiler.compile 
     end
 
-#     def delegate
-#       return unless arguments.delegatable? 
-#       delegate_to_forwardee
-#       true
-#     end
-# 
-#     def delegate_all
-#       return unless arguments.all?
-#       delegate_all_to_forwardee
-#       true
-#     end
-# 
-#     def delegate_all_to_forwardee
-#       arguments.message.each do | msg |
-#         forwardee.module_eval(
-#           "def #{msg} *args, &blk; #{arguments.target}.#{msg}( *args, &blk ) end",
-#           __FILE__,
-#           __LINE__
-#         )
-#       end
-#     end
-# 
-# 
-#     def delegate_chain
-#       return unless arguments.chain?
-#       delegate_to_chain
-#       true
-#     end
-# 
-#     def delegate_to_chain
-#       tltion = arguments.translation arguments.message
-#       forwardee.module_eval(
-#         "def #{arguments.message} *args, &blk; #{arguments.target.join(".")}.#{tltion}( *args, &blk ) end",
-#           __FILE__,
-#           __LINE__
-#       )
-#     end
-# 
-#     def delegate_object
-#       return unless arguments.custom_target?
-#       Meta.new( forwardee, arguments ).forward_object
-#       true
-#     end
-#       
-#     def delegate_to_forwardee
-#       tltion = arguments.translation arguments.message
-#       forwardee.module_eval(
-#         "def #{arguments.message} *args, &blk; #{arguments.target}.#{tltion}( #{arguments.serialized_params}*args, &blk ) end",
-#         __FILE__,
-#         __LINE__
-#       )
-#     end
-# 
     def initialize forwardee
       @forwardee = forwardee
     end
